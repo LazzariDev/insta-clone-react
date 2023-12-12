6baf6241604590182019ee81f3cb965264df84ab
Emanuele <e.lazzari@geosystems.it>
2023-12-12 09:07:04 +0100

    ProfilePage > ProfileHeader & ProfileTabs

---
+++ src/components/Profile/ProfilePost.jsx
@@ -0,0 +1,6 @@
+
+export default function ProfilePost() {
+    return (
+        <div>ProfilePost</div>
+    )
+}
---
+++ src/components/Profile/ProfilePosts.jsx
@@ -0,0 +1,6 @@
+
+export default function ProfilePosts() {
+    return (
+        <div>ProfilePosts</div>
+    )
+}
