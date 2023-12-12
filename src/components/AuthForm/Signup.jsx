49eb7fc4ca552f7f1d1ac7c36513c25e803ba570
Emanuele <e.lazzari@geosystems.it>
2023-12-12 16:30:35 +0100

    Logout, AuthStore & useAuthStore, Navbar

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,9 +1,10 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { Alert, AlertIcon, Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { useState } from "react";
+import useSignUpWithEmailAndPassword from "../../hooks/useSignUpWithEmailAndPassword";
 
 export default function Signup() {
-    const [isLogin, setIsLogin] = useState(true);
+    // const [isLogin, setIsLogin] = useState(true);
     const [showPassword, setShowPassword] = useState(false);
     const [inputs, setInputs] = useState({
         fullName: "",
@@ -12,6 +13,8 @@ export default function Signup() {
         password: ""
     });
 
+    const { loading, error, signup } = useSignUpWithEmailAndPassword()
+    
     return (
         <>
             < Input onChange={(e) => setInputs({...inputs, fullName: e.target.value})} placeholder="Full Name" fontSize={14} size={"sm"} type="text" value={inputs.fullName} />
@@ -25,7 +28,15 @@ export default function Signup() {
                     </Button>
                 </InputRightElement>
             </InputGroup>
-            <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+
+            {error && (
+                <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+                    < AlertIcon fontSize={12} />
+                    {error.message}
+                </Alert>
+            )}
+
+            <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} onClick={() => signup(inputs)} isLoading={loading}>
                 Sign Up
             </Button>
         </>
