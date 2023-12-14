ee3bddabfbb92a6bddc33465d5bdff828544cb5f
Emanuele <e.lazzari@geosystems.it>
2023-12-14 10:41:50 +0100

    Fetch users posts

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,12 +1,16 @@
-import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react";
+import { Avatar, Box, Button, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, VStack, useDisclosure } from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Comment from "../Comment/Comment";
 import PostFooter from "../FeedPosts/PostFooter";
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from "../../store/AuthStore";
 
-export default function ProfilePost({img}) {
+export default function ProfilePost({post}) {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const userProfile = useUserProfileStore((state) => state.userProfile);
+    const authUser = useAuthStore((state) => state.user);
 
     return (
         <>
@@ -16,19 +20,19 @@ export default function ProfilePost({img}) {
                         <Flex>
                             < AiFillHeart size={20} />
                             <Text fontWeight={"bold"} ml={2}>
-                                7
+                                {post.likes.length}
                             </Text>
                         </Flex>
                         <Flex>
                             < FaComment size={20} />
                             <Text fontWeight={"bold"} ml={2}>
-                                7
+                                {post.comments.length}
                             </Text>
                         </Flex>
                     </Flex>
                 </Flex>
 
-                < Image src={img} alt="profile post" w={"100%"} h={"100%"} objectFit={"cover"} />
+                < Image src={post.imageURL} alt="profile post" w={"100%"} h={"100%"} objectFit={"cover"} />
             </GridItem>
 
             <Modal isOpen={isOpen} onClose={onClose} isCentered={true} size={{base:"3xl", md:"5xl"}}>
@@ -36,22 +40,24 @@ export default function ProfilePost({img}) {
                 <ModalContent>
                     < ModalCloseButton />
                     <ModalBody bg={"black"} pb={5}>
-                        <Flex gap={4} w={{base: "90%", sm:"70%", md: "full"}} mx={"auto"}>
-                            <Box borderRadius={4} overflow={"hidden"} border={"1px solid"} borderColor={"whiteAlpha.300"} flex={1.5}>
-                                < Image src={img} alt="Profile post" />
-                            </Box>
+                        <Flex gap={4} w={{base: "90%", sm:"70%", md: "full"}} mx={"auto"} maxH={"90vh"} minH={"50vh"}>
+                            <Flex borderRadius={4} overflow={"hidden"} border={"1px solid"} borderColor={"whiteAlpha.300"} flex={1.5} justifyContent={"center"} alignItems={"center"}>
+                                < Image src={post.imageURL} alt="Profile post" />
+                            </Flex>
                             <Flex flex={1} flexDir={"column"} px={10} display={{base:"none", md:"flex"}}>
                                 <Flex alignItems={"center"} justifyContent={"space-between"}>
 
                                     <Flex alignItems={"center"} gap={4}>
-                                        <Avatar src="/profilepic.png" size={"sm"} name="random guy" />
+                                        <Avatar src={userProfile.profilePicURL} size={"sm"} name="random guy" />
                                         <Text fontWeight={"bold"} fontSize={12}>
-                                            randomguy
+                                            {userProfile.username}
                                         </Text>
                                     </Flex>
-                                    <Box _hover={{bg:"whiteAlpha.300", color:"red.600"}} borderRadius={4} p={1}>
-                                        <MdDelete size={20}  cursor={"pointer"} />
-                                    </Box>
+                                    {authUser?.uid === userProfile.uid && (
+                                        <Button size={"sm"} bg={"transparent"}  _hover={{bg:"whiteAlpha.300", color:"red.600"}} borderRadius={4} p={1}>
+                                            <MdDelete size={20}  cursor={"pointer"} />
+                                        </Button>
+                                    )}
                                 </Flex>
 
                                 < Divider my={4} bg={"gray.500"} />
@@ -59,7 +65,6 @@ export default function ProfilePost({img}) {
                                 <VStack w={"full"} alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
                                     <Comment createdAt="1d ago" username="randomguy" profilePic="/profilepic.png" text={"Dummy images from unsplash"} />
                                     <Comment createdAt="12h ago" username="someguy" profilePic={"https://bit.ly/dan-abramov"} text={"Nice pic"} />
-                                    <Comment createdAt="3h ago" username="somefriend" profilePic={"https://bit.ly/kent-c-dodds"} text={"This post is lit"} />
                                 </VStack>
                                 < Divider my={4} bg={"gray.800"} />
                                 < PostFooter isProfilePage={true}/>
